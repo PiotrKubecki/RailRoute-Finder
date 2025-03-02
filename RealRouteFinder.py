5d46fee08443223a5a20cacda891cb1869867c90
PiotrKubecki <piotrkubecki01@gmail.com>
2025-03-02 14:26:34 +0100

    Restructuring RealRouteFinder into two separate classes. Moving functions connected with webscraping to new class.

--- RealRouteFinder.py
+++ RealRouteFinder.py
@@ -1,20 +1,18 @@
 import os
 import time
 from datetime import datetime
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from webdriver_manager.chrome import ChromeDriverManager
 import logging
+from WebDriverManager import WebDriverManager
 
 
 class RealRouteFinder:
     def __init__(self):
         self.driver_path = self.get_chrome_driver_path()
-        self.driver = None
+        self.driver_manager = WebDriverManager(self.driver_path)
 
     def get_chrome_driver_path(self):
         driver_path = "chromedriver.exe"
@@ -22,56 +20,22 @@ class RealRouteFinder:
             driver_path = ChromeDriverManager().install()
         return driver_path
 
-    def start_driver(self):
-        options = Options()
-        options.add_argument('--headless')
-        self.driver = webdriver.Chrome(service=Service(self.driver_path), options=options)
-
-    def stop_driver(self):
-        if self.driver:
-            self.driver.quit()
-            self.driver = None
-
-    def wait_for_element(self, by_strategy, locator, timeout=10):
-        return WebDriverWait(self.driver, timeout).until(
-            ec.presence_of_element_located((by_strategy, locator))
-        )
-
-    def click_element(self, by_strategy, locator, timeout=10):
-        element = WebDriverWait(self.driver, timeout).until(
-            ec.element_to_be_clickable((by_strategy, locator))
-        )
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
-        time.sleep(0.5)
-        element.click()
-
-    def enter_text(self, by_strategy, locator, text, timeout=10):
-        element = self.wait_for_element(by_strategy, locator, timeout)
-        element.clear()
-        element.send_keys(text)
-
     def set_date_by_clicking(self, date_string):
-        try:
-            target_date = datetime.strptime(date_string, "%Y-%m-%d").date()
+        target_date = datetime.strptime(date_string, "%Y-%m-%d").date()
+        current_date = datetime.now().date()
+        days_difference = (target_date - current_date).days
 
-            current_date = datetime.now().date()
-            days_difference = (target_date - current_date).days
+        button_xpath = '//*[@id="wyszukiwarka"]/form/div[8]/div[2]/div[2]/button[2]'
+        date_change_button = self.driver_manager.wait_for_element(By.XPATH, button_xpath)
+        self.driver_manager.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", date_change_button)
+        time.sleep(0.5)
 
-            button_xpath = '//*[@id="wyszukiwarka"]/form/div[8]/div[2]/div[2]/button[2]'
+        if not date_change_button.is_enabled() or not date_change_button.is_displayed():
+            return
 
-            date_change_button = self.wait_for_element(By.XPATH, button_xpath)
-            self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", date_change_button)
-            time.sleep(0.5)
-
-            if not date_change_button.is_enabled() or not date_change_button.is_displayed():
-                return
-
-            for _ in range(abs(days_difference)):
-                date_change_button.click()
-                time.sleep(0.1)
-
-        except Exception as e:
-            logging.info(f"Error {e} occured")
+        for _ in range(abs(days_difference)):
+            date_change_button.click()
+            time.sleep(0.1)
 
     def select_checkboxes(self, options_to_select):
         checkbox_xpaths = {
@@ -80,70 +44,59 @@ class RealRouteFinder:
             'facilities for disabled people': "//*[@id='collapseOne']/fieldset/div[3]/div/label[2]/div",
             'facilities for people with children': "//*[@id='collapseOne']/fieldset/div[4]/div/label[2]/div"
         }
-        try:
-            for option in options_to_select:
-                if option in checkbox_xpaths:
-                    xpath = checkbox_xpaths[option]
-                    checkbox = self.wait_for_element(By.XPATH, xpath)
-                    if not checkbox.is_selected():
-                        checkbox.click()
-                else:
-                    print(f"Opcja '{option}' nie jest rozpoznawana.")
-        except Exception as e:
-            logging.info(f"Error {e} occured")
+        for option in options_to_select:
+            if option in checkbox_xpaths:
+                xpath = checkbox_xpaths[option]
+                checkbox = self.driver_manager.wait_for_element(By.XPATH, xpath)
+                if not checkbox.is_selected():
+                    checkbox.click()
 
     def anchor_close(self):
-        try:
-            self.click_element(By.XPATH, "//*[@id='yb_anchor_wrapper']/span", timeout=10)
-        except Exception as e:
-            logging.info(f"Error {e} occured")
+        self.driver_manager.click_element(By.XPATH, "//*[@id='yb_anchor_wrapper']/span", timeout=10)
 
     def find_real_routes(self, start_station, end_station, date_string, time_value, checkbox_options):
-        self.start_driver()
-        self.driver.get("https://rozklad-pkp.pl")
+        self.driver_manager.start_driver()
+        self.driver_manager.driver.get("https://rozklad-pkp.pl")
 
-        try:
-            self.click_element(By.XPATH, "//*[@id='qc-cmp2-ui']/div[2]/div/button[2]")
-            self.enter_text(By.XPATH, "//*[@id='from-station']", start_station)
-            self.enter_text(By.XPATH, "//*[@id='to-station']", end_station)
-            self.enter_text(By.XPATH, "//*[@id='hour']", time_value)
+        self.driver_manager.click_element(By.XPATH, "//*[@id='qc-cmp2-ui']/div[2]/div/button[2]")
+        self.driver_manager.enter_text(By.XPATH, "//*[@id='from-station']", start_station)
+        self.driver_manager.enter_text(By.XPATH, "//*[@id='to-station']", end_station)
+        self.driver_manager.enter_text(By.XPATH, "//*[@id='hour']", time_value)
 
-            self.anchor_close()
-            self.set_date_by_clicking(date_string)
+        self.anchor_close()
+        self.set_date_by_clicking(date_string)
 
-            if checkbox_options:
-                self.select_checkboxes(checkbox_options)
-            self.click_element(By.XPATH, "//*[@id='singlebutton']")
-
-        except Exception as e:
-            logging.info(f"Error {e} occured")
+        if checkbox_options:
+            self.select_checkboxes(checkbox_options)
+        self.driver_manager.click_element(By.XPATH, "//*[@id='singlebutton']")
 
         connections = []
+        no_results_xpath = "//div[contains(@class, 'no-result')]"
         try:
-            no_results_xpath = "//div[contains(@class, 'no-result')]"
-            WebDriverWait(self.driver, 1).until(
-                ec.presence_of_element_located((By.XPATH, no_results_xpath)))
-            self.stop_driver()
+            WebDriverWait(self.driver_manager.driver, 1).until(
+                ec.presence_of_element_located((By.XPATH, no_results_xpath))
+            )
+            self.driver_manager.stop_driver()
             return None
-
         except Exception as e:
-            logging.info(f"Error {e} occured")
+            logging.warning(f"Error {e} occurred")
 
         for i in range(1, 3):
             try:
                 base_xpath = f"//*[@id='focus_guiVCtrl_connection_detailsOut_select_C0-{i}']"
-                transfer_amount = self.wait_for_element(By.XPATH, base_xpath + "/td[6]").text
-                duration = self.wait_for_element(By.XPATH, base_xpath + "/td[5]").text
-                departure_time = self.wait_for_element(By.XPATH, base_xpath + "/td[4]/p[1]/span[1]/span[3]").text
+                transfer_amount = self.driver_manager.wait_for_element(By.XPATH, base_xpath + "/td[6]").text
+                duration = self.driver_manager.wait_for_element(By.XPATH, base_xpath + "/td[5]").text
+                departure_time = self.driver_manager.wait_for_element(
+                    By.XPATH, base_xpath + "/td[4]/p[1]/span[1]/span[3]").text
                 connections.append({
                     'transfers': transfer_amount,
                     'departure': departure_time,
                     'duration': duration
                 })
             except Exception as e:
-                logging.info(f"Error {e} occured")
+                logging.warning(f"Error {e} occurred")
 
-        self.stop_driver()
+        self.driver_manager.stop_driver()
         return connections if connections else None
 
     def find_connections(self, start_stations, end_stations, date_string, time_value, checkbox_options=None):
